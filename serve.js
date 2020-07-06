326a2d3f779aa53214e0f7a26749f91fd2a4be7f
Tahara <33447713+Tahara256@users.noreply.github.com>
2020-07-06 11:58:03 +0900

    Add omikuji analytics

--- serve.js
+++ serve.js
@@ -3,14 +3,24 @@ const app = express();
 const port = 3000;
 
 fortunes = ['大吉', '吉', '中吉', '小吉', '末吉', '凶'];
+statistics = {};
+
+for (f of fortunes) {
+  statistics[f] = 0;
+}
 
 app.get('/omikuji/', (req, res) => {
   randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+  statistics[randomFortune]++;
 
   result = {
     fortune: randomFortune
   };
   res.json(result);
+});
+
+app.get('/omikuji/analytics/', (req, res) => {
+  res.json(statistics);
 });
 
 app.listen(port, () => {
